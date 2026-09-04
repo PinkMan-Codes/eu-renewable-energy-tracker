c36f06c4087c7827b0040b611012157764964617
PinkMan-Codes <alaes@iscte-iul.pt>
2026-09-04 15:59:32 +0100

    Add forecast analysis and update ETL

--- etl/extract.py
+++ etl/extract.py
@@ -4,7 +4,9 @@ import pandas as pd
 
 sdg = eurostat.get_data_df("sdg_07_40")
 env_air = eurostat.get_data_df("env_air_gge")
+pop = eurostat.get_data_df("demo_pjan")
 
 #file save
 sdg.to_csv("data/raw/sdg_07_40.csv", index=False)
 env_air.to_csv("data/raw/env_air_.csv", index=False)
+pop.to_csv("data/raw/pop.csv", index=False)
